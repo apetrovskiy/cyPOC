ad6525dcefe7396ea49652ad5163c93a937d5320
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-06-01 22:13:41 +0300

    initial setup

---
+++ cypress/integration/url_constants.ts
@@ -0,0 +1,5 @@
+namespace Constants {
+    enum Urls {
+        LoginPage = "/oauth/ngs/login" // "https://account.privo.com/oauth/ngs/login"
+    }
+}
---
+++ cypress/integration/url_constants_js.js
@@ -0,0 +1,4 @@
+
+const Urls = Object.freeze({ LOGIN_PAGE: '/oauth/ngs/login' }); // 'https://account.privo.com/oauth/ngs/login' });
+
+module.exports = { Urls };
